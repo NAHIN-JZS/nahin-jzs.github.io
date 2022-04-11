fcfb4323b6d5cb4c19f8de8103421d4a2b4498cd
NAHIN-JZS <syednahinhossain@gmail.com>
2022-04-11 16:21:32 +0600

    sending email to me is activated

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,26 @@ AOS.init({
 });
 
 // Add your javascript here
+
+//  Bind the event handler to the "submit" JavaScript event
+$('form').submit(function () {
+
+  // Get the Login Name value and trim it
+  var fname = $.trim($('#fname').val());
+  var lname = $.trim($('#lname').val());
+  var phon =$.trim($('#phon').val());
+
+  // Check if empty of not
+  if (fname === '') {
+      alert('First name is empty.');
+      return false;
+  }
+  else if (lname === '') {
+      alert('Last Name is empty.');
+      return false;
+  }
+  else if (phon === '') {
+      alert('Phone is empty.');
+      return false;
+  }
+});
